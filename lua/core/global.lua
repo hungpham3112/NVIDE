705c1bc54c9b5a65bbca16f7bf320ff0c59b47eb
hungpham3112 <phamhung20022015@gmail.com>
2025-09-26 04:11:29 +0700

    feat(core): remove global keymap

--- lua/core/global.lua
+++ lua/core/global.lua
@@ -1,4 +1,9 @@
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.mapleader = " "                            -- Map leader to Space for convenience
+vim.g.mapleader = " " -- Map leader to Space for convenience
+vim.keymap.del('n', "grn")
+vim.keymap.del('n', "gra")
+vim.keymap.del('n', "grt")
+vim.keymap.del('n', "gri")
+vim.keymap.del('n', "grr")
