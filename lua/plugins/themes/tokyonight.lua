50f758ca2e2179437ae68390c278e19a01ac762b
hungpham3112 <phamhung20022015@gmail.com>
2025-10-30 16:25:41 +0700

    feat(plugins, core): add transparent

--- lua/plugins/themes/tokyonight.lua
+++ lua/plugins/themes/tokyonight.lua
@@ -2,7 +2,16 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme('tokyonight-storm')
+    opts = {
+        style = "moon",
+        -- transparent = true,
+        -- on_colors = function(colors)
+        --     colors.bg_statusline = "NONE"
+        --     colors.bg_statusline_inactive = "NONE"
+        -- end,
+    },
+    config = function(_, opts)
+        require("tokyonight").setup(opts)
+        vim.cmd.colorscheme("tokyonight")
     end,
 }
