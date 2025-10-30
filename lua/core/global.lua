f817c227f5104012a846e8b82c13faad219757a2
hungpham3112 <phamhung20022015@gmail.com>
2025-10-30 16:44:39 +0700

    feat(core): add smart buffer close

--- lua/core/global.lua
+++ lua/core/global.lua
@@ -7,3 +7,13 @@ vim.keymap.del('n', "gra")
 vim.keymap.del('n', "grt")
 vim.keymap.del('n', "gri")
 vim.keymap.del('n', "grr")
+
+function _G.smart_bdelete()
+    local cur_buf = vim.api.nvim_get_current_buf()
+    vim.cmd('bnext') -- switch to next buffer
+    if cur_buf ~= vim.api.nvim_get_current_buf() then
+        vim.cmd('bdelete ' .. cur_buf)
+    end
+end
+
+vim.keymap.set('n', '<leader>bd', ':lua smart_bdelete()<CR>', { desc = 'Smart delete buffer' })
